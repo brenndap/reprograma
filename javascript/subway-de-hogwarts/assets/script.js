32a418f71d5c6a68db5bbe9a79e6f7d84da25072
Brennda <brennda.perola@gmail.com>
2019-04-22 14:15:24 -0300

    adiciona correções

--- javascript/subway-de-hogwarts/assets/script.js
+++ javascript/subway-de-hogwarts/assets/script.js
@@ -1,39 +1,44 @@
 window.addEventListener('DOMContentLoaded', function () {
     const houseOptions = [];
-    houseOptions["Grifinória"] = [];
-    houseOptions["Grifinória"]["items"] = ["frances", "suico", "rosbife", "mostarda", "tostado"];
-    houseOptions["Grifinória"]["score"] = 0;
-    houseOptions["Grifinória"]["gif"] = 'https://media.giphy.com/media/40KXzKllSl6ve/giphy.gif';
-    houseOptions["Sonserina"] = [];
-    houseOptions["Sonserina"]["items"] = ["italiano", "mussarela", "presunto", "maionese", "frio"];
-    houseOptions["Sonserina"]["score"] = 0;
-    houseOptions["Sonserina"]["gif"] = 'https://media.giphy.com/media/4E1Yw7cA5DYwU/giphy.gif';
-    houseOptions["Lufa-lufa"] = [];
-    houseOptions["Lufa-lufa"]["items"] = ["integral", "cheddar", "jaca", "barbecue", "tostado"];
-    houseOptions["Lufa-lufa"]["score"] = 0;
-    houseOptions["Lufa-lufa"]["gif"] = 'https://media.giphy.com/media/1UAIoVWRhtjhe/giphy.gif';
-    houseOptions["Corvinal"] = [];
-    houseOptions["Corvinal"]["items"] = ["granola", "vegano", "atum", "parmesao", "frio"];
-    houseOptions["Corvinal"]["score"] = 0;
-    houseOptions["Corvinal"]["gif"] = 'https://media.giphy.com/media/BxYmEq7Ksqlbi/giphy.gif';
+    // popular os radios no HTML pelo JS - innerText
+    houseOptions["gryffindor"] = [];
+    houseOptions["gryffindor"]["name"] = "Grifinória";
+    houseOptions["gryffindor"]["items"] = ["frances", "suico", "rosbife", "mostarda", "tostado"];
+    houseOptions["gryffindor"]["score"] = 0;
+    houseOptions["gryffindor"]["gif"] = 'https://media.giphy.com/media/40KXzKllSl6ve/giphy.gif';
+    houseOptions["slytherin"] = [];
+    houseOptions["slytherin"]["name"] = "Sonserina";    
+    houseOptions["slytherin"]["items"] = ["italiano", "mussarela", "presunto", "maionese", "frio"];
+    houseOptions["slytherin"]["score"] = 0;
+    houseOptions["slytherin"]["gif"] = 'https://media.giphy.com/media/4E1Yw7cA5DYwU/giphy.gif';
+    houseOptions["hufflepuff"] = [];
+    houseOptions["hufflepuff"]["name"] = "Lufa-lufa";
+    houseOptions["hufflepuff"]["items"] = ["integral", "cheddar", "jaca", "barbecue", "tostado"];
+    houseOptions["hufflepuff"]["score"] = 0;
+    houseOptions["hufflepuff"]["gif"] = 'https://media.giphy.com/media/1UAIoVWRhtjhe/giphy.gif';
+    houseOptions["ravenclaw"] = [];
+    houseOptions["ravenclaw"]["name"] = "Corvinal";
+    houseOptions["ravenclaw"]["items"] = ["granola", "vegano", "atum", "parmesao", "frio"];
+    houseOptions["ravenclaw"]["score"] = 0;
+    houseOptions["ravenclaw"]["gif"] = 'https://media.giphy.com/media/BxYmEq7Ksqlbi/giphy.gif';
     const submit = document.getElementById('submit');
-    const bread = document.getElementsByName("pao"); 
-    const cheese = document.getElementsByName("queijo");
-    const protein = document.getElementsByName("proteina");
-    const sauce = document.getElementsByName("molho");
-    const type = document.getElementsByName('tipo');
-    const finalResult = document.getElementById("resultado");
+    const bread = document.getElementsByName("bread"); 
+    const cheese = document.getElementsByName("chesse");
+    const protein = document.getElementsByName("protein");
+    const sauce = document.getElementsByName("sauce");
+    const type = document.getElementsByName('type');
+    const finalResult = document.getElementById("result");
     const img = document.createElement('img');
     let itemsList = [];
 
     function getRadioValue(radioGroup){
         for(let i = 0; i < radioGroup.length; i++ ){
             if(radioGroup[i].checked){
-                return itemsList.push(radioGroup[i].value);
+                itemsList.push(radioGroup[i].value);
             }
         }
         
-    }
+    };
 
     function addScore(itemsList){
         for(let i = 0; i < itemsList.length; i++ ){
@@ -59,18 +64,14 @@ window.addEventListener('DOMContentLoaded', function () {
                 return key;
             }
         };
-    }
+    };
     
     submit.addEventListener("click", function(){
 
-        getRadioValue(bread);
-        getRadioValue(cheese);
-        getRadioValue(protein);
-        getRadioValue(sauce);
-        getRadioValue(type);
+        getRadioValue(bread, cheese, protein, sauce, type);
         addScore(itemsList);
         let winnerHouse = getMaxScoreHouse()
-        finalResult.innerHTML = winnerHouse;
+        finalResult.innerHTML = houseOptions[winnerHouse]["name"];
         img.src = houseOptions[winnerHouse]["gif"];
         finalResult.append(img);
    
