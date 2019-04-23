28a8090fb9b929167a54fab3b7a5e725889a455c
Brennda <brennda.perola@gmail.com>
2019-04-23 10:58:04 -0300

    adiciona classe

--- javascript/subway-de-hogwarts/assets/script.js
+++ javascript/subway-de-hogwarts/assets/script.js
@@ -1,82 +1,91 @@
 window.addEventListener('DOMContentLoaded', function () {
-    const houseOptions = [];
-    // popular os radios no HTML pelo JS - innerText
-    houseOptions["gryffindor"] = [];
-    houseOptions["gryffindor"]["name"] = "Grifinória";
-    houseOptions["gryffindor"]["items"] = ["frances", "suico", "rosbife", "mostarda", "tostado"];
-    houseOptions["gryffindor"]["score"] = 0;
-    houseOptions["gryffindor"]["gif"] = 'https://media.giphy.com/media/40KXzKllSl6ve/giphy.gif';
-    houseOptions["slytherin"] = [];
-    houseOptions["slytherin"]["name"] = "Sonserina";    
-    houseOptions["slytherin"]["items"] = ["italiano", "mussarela", "presunto", "maionese", "frio"];
-    houseOptions["slytherin"]["score"] = 0;
-    houseOptions["slytherin"]["gif"] = 'https://media.giphy.com/media/4E1Yw7cA5DYwU/giphy.gif';
-    houseOptions["hufflepuff"] = [];
-    houseOptions["hufflepuff"]["name"] = "Lufa-lufa";
-    houseOptions["hufflepuff"]["items"] = ["integral", "cheddar", "jaca", "barbecue", "tostado"];
-    houseOptions["hufflepuff"]["score"] = 0;
-    houseOptions["hufflepuff"]["gif"] = 'https://media.giphy.com/media/1UAIoVWRhtjhe/giphy.gif';
-    houseOptions["ravenclaw"] = [];
-    houseOptions["ravenclaw"]["name"] = "Corvinal";
-    houseOptions["ravenclaw"]["items"] = ["granola", "vegano", "atum", "parmesao", "frio"];
-    houseOptions["ravenclaw"]["score"] = 0;
-    houseOptions["ravenclaw"]["gif"] = 'https://media.giphy.com/media/BxYmEq7Ksqlbi/giphy.gif';
+    class createHouse {
+        constructor(name, items, score, gif) {
+            this.name = name;
+            this.items = items;
+            this.score = score;
+            this.gif = gif;
+        }
+        addScore() {
+            this.score += 1
+        }
+    }
+    const houses = [
+        new createHouse("Grifinória", ["frances", "suico", "rosbife", "mostarda", "tostado"], 0, 'https://media.giphy.com/media/40KXzKllSl6ve/giphy.gif'),
+        new createHouse("Sonserina", ["italiano", "mussarela", "presunto", "maionese", "frio"], 0, 'https://media.giphy.com/media/4E1Yw7cA5DYwU/giphy.gif'),
+        new createHouse("Lufa-Lufa", ["integral", "cheddar", "jaca", "barbecue", "tostado"], 0, 'https://media.giphy.com/media/1UAIoVWRhtjhe/giphy.gif'),
+        new createHouse("Corvinal", ["granola", "vegano", "atum", "parmesao", "frio"], 0, 'https://media.giphy.com/media/BxYmEq7Ksqlbi/giphy.gif')
+    ]
+
     const submit = document.getElementById('submit');
-    const bread = document.getElementsByName("bread"); 
-    const cheese = document.getElementsByName("chesse");
+    const bread = document.getElementsByName("bread");
+    const cheese = document.getElementsByName("cheese");
     const protein = document.getElementsByName("protein");
     const sauce = document.getElementsByName("sauce");
     const type = document.getElementsByName('type');
     const finalResult = document.getElementById("result");
     const img = document.createElement('img');
     let itemsList = [];
-
-    function getRadioValue(radioGroup){
-        for(let i = 0; i < radioGroup.length; i++ ){
-            if(radioGroup[i].checked){
+    
+    function getRadioValue(radioGroup) {
+        for (let i = 0; i < radioGroup.length; i++) {
+            console.log(radioGroup.length);
+            if (radioGroup[i].checked) {
                 itemsList.push(radioGroup[i].value);
             }
         }
+
+    };
+
+    function score(itemsList) {
+
+        for (let i = 0; i < houses.length; i++) {
+            let a = houses[i].items;
+            if (a.includes(itemsList[i])) {
+                houses[i].addScore();
+                console.log(houses[i])
+            }
+        };
+
+
+    };
+
+    function getMaxScoreHouse() {
+        let scoreList = [];
+        for (let i = 0; i < houses.length; i++) {
+            scoreList.push(houses[i].score);
+        }
+        console.log(scoreList);
+        let maxScore = scoreList.reduce(function (a, b) { return Math.max(a, b); });
+        for (let i = 0; i < houses.length; i++) {
+            if (houses[i].score == maxScore) {
+                return houses[i].name;
+            }
+        };
         
     };
 
-    function addScore(itemsList){
-        for(let i = 0; i < itemsList.length; i++ ){
-            for(let key in houseOptions){
-                if(houseOptions[key]["items"].includes(itemsList[i])){
-                    houseOptions[key]["score"] += 1;
-                }
+    function setGif(){
+        for(let i = 0; i < houses.length; i++){
+            if(getMaxScoreHouse() == houses[i].name){
+                return houses[i].gif;
             }
-        };
-   
-    };
-
-    function getMaxScoreHouse(){
-        let scoreList = [];
-        for(let key in houseOptions){
-            scoreList.push(houseOptions[key]["score"]);
         }
-        let maxScore = scoreList.reduce(function(a, b) {
-            return Math.max(a, b);
-        });
-        for(let key in houseOptions){
-            if(houseOptions[key]["score"] == maxScore){
-                return key;
-            }
-        };
-    };
-    
-    submit.addEventListener("click", function(){
+    }
 
-        getRadioValue(bread, cheese, protein, sauce, type);
-        addScore(itemsList);
-        let winnerHouse = getMaxScoreHouse()
-        finalResult.innerHTML = houseOptions[winnerHouse]["name"];
-        img.src = houseOptions[winnerHouse]["gif"];
+    submit.addEventListener("click", function () {
+        
+        getRadioValue(bread);
+        getRadioValue(cheese);
+        getRadioValue(protein);
+        getRadioValue(sauce);
+        getRadioValue(type);
+        score(itemsList);
+        finalResult.innerHTML = getMaxScoreHouse();
+        img.src = setGif();
         finalResult.append(img);
-   
-
     });
 
 });
+
 
